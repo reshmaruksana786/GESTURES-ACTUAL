b7b580304de3473811d515c2fdbbb5dc9e90c859
reshma <reshmaruksana786786@gmail.com>
2020-01-24 18:31:39 +0530

    Initial Commit

---
+++ GESTURES ACTUAL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GESTURES ACTUAL
+//
+//  Created by Syed.Reshma Ruksana on 24/01/20.
+//  Copyright © 2020 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
